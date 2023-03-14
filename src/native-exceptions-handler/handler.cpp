0c25af33ccbb736012bd386bc110fb932eff3b63
JerryDing <deanroom@gmail.com>
2023-03-15 00:13:52 +0800

    feat:add demo

--- src/native-exceptions-handler/handler.cpp
+++
@@ -1,9 +0,0 @@
-#include "handler/handler_main.h"
-
-using namespace crashpad;
-
-int main(int argc, char **argv) {
-    auto sources = new UserStreamDataSources();
-    HandlerMain(argc, argv, sources);
-    return 0;
-}
--- src/native-exceptions/main.cpp
+++ src/native-exceptions/main.cpp
@@ -4,10 +4,77 @@
 #include "CLI/Config.hpp"
 #include "native-exceptions.h"
 
+#include <stddef.h>
+#include <string.h>
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+#include <client/crash_report_database.h>
+#include <client/settings.h>
+#include <client/crashpad_client.h>
+#include <client/crashpad_info.h>
+
+std::unique_ptr<crashpad::CrashReportDatabase> database;
+
+static bool
+startCrashHandler(std::string const& url, std::string const& handler_path,
+    std::string const& db_path)
+{
+    using namespace crashpad;
+
+    std::map<std::string, std::string> annotations;
+    std::vector<std::string> arguments;
+    bool rc;
+
+    /*
+     * THE FOLLOWING ANNOTATIONS MUST BE SET.
+     *
+     * Backtrace supports many file formats. Set format to minidump
+     * so it knows how to process the incoming dump.
+     */
+    annotations["format"] = "minidump";
+
+    /*
+     * REMOVE THIS FOR ACTUAL BUILD.
+     *
+     * We disable crashpad rate limiting for this example.
+     */
+    arguments.push_back("--no-rate-limit");
+
+    base::FilePath db(db_path);
+    base::FilePath handler(handler_path);
+
+    database = crashpad::CrashReportDatabase::Initialize(db);
+
+    if (database == nullptr || database->GetSettings() == NULL)
+        return false;
+
+    /* Enable automated uploads. */
+    database->GetSettings()->SetUploadsEnabled(false);
+
+    return CrashpadClient{}.StartHandler(
+        handler, db, db, url, annotations, arguments, false, false, {}
+    );
+}
 
 int main() {
 
     CLI::App app{"Native Exception Demo"};
+
+    std::string url("");
+
+    std::string handler_path("crashpad/handler/handler");
+
+   
+    std::string db_path("./dmpdb");
+
+    std::cout << "Crashpad URL: " << url << '\n';
+    std::cout << "Crashpad handler: " << handler_path << '\n';
+    std::cout << "Crashpad database: " << db_path << '\n';
+    std::cerr << startCrashHandler(url, handler_path, db_path) << '\n';
+
 
     //ErrorNumber error_number{ErrorNumber::NormalThrow};
     int error_number = 0;
